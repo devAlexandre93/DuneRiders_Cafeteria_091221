8ce8b2fa86f32c9e598cac378c4ff6335cecf95c
aanan <anania.alexandre@gmail.com>
2021-12-12 20:54:41 +0100

    Minors changes on avatars and buttons

--- app/src/main/java/fr/epsi/cafeteria/GroupActivity.kt
+++ app/src/main/java/fr/epsi/cafeteria/GroupActivity.kt
@@ -18,7 +18,7 @@ class GroupActivity : BaseActivity() {
         buttonStudent1.setOnClickListener(View.OnClickListener {
             val newIntent = Intent(application,DetailsActivity::class.java)
             newIntent.putExtra("title","Clément")
-            newIntent.putExtra("urlImage","https://avatars.dicebear.com/v2/male/b28fbff8404004995e0db18a41465e54.png")
+            newIntent.putExtra("urlImage","https://avatars.dicebear.com/v2/male/e3795a61ccf739c63f042fa1d8f01fa6.png")
             newIntent.putExtra("infos","28 ans")
             newIntent.putExtra("name","Clément Escande")
             newIntent.putExtra("email","clement.escande@epsi.fr")
@@ -28,7 +28,7 @@ class GroupActivity : BaseActivity() {
         buttonStudent2.setOnClickListener(View.OnClickListener {
             val newIntent = Intent(application,DetailsActivity::class.java)
             newIntent.putExtra("title","Yahya Qaddouri")
-            newIntent.putExtra("urlImage","https://avatars.dicebear.com/v2/male/7f0f8030f7a23375644a4e9ea46916b0.png")
+            newIntent.putExtra("urlImage","https://avatars.dicebear.com/v2/male/28665fe6eaa02541e25282e0a56c61aa.png")
             newIntent.putExtra("infos","22 ans")
             newIntent.putExtra("name","Yahya Qaddouri")
             newIntent.putExtra("email","yahya.qaddouri@epsi.fr")
@@ -38,7 +38,7 @@ class GroupActivity : BaseActivity() {
         buttonStudent3.setOnClickListener(View.OnClickListener {
             val newIntent = Intent(application,DetailsActivity::class.java)
             newIntent.putExtra("title","Alexandre")
-            newIntent.putExtra("urlImage","https://avatars.dicebear.com/v2/male/94ef6d97cde99e62e2b8fc53130a0198.png")
+            newIntent.putExtra("urlImage","https://avatars.dicebear.com/v2/male/ad5ee5c403181cc1604f8ed9ff090e8f.png")
             newIntent.putExtra("infos","28 ans")
             newIntent.putExtra("name","Alexandre Anania")
             newIntent.putExtra("email","alexandre.anania@epsi.fr")
