b538b0ce722428a0989f7d99ddfe6b9eb4a7dd61
aanan <anania.alexandre@gmail.com>
2021-12-12 23:22:25 +0100

    Minor changes for step 1 and start of WebService part

--- app/src/main/java/fr/epsi/cafeteria/Product.kt
+++
@@ -1,4 +0,0 @@
-package fr.epsi.cafeteria
-
-class Product(val categoryId:String,val name:String,val productsUrl:String) {
-}
--- app/src/main/java/fr/epsi/cafeteria/WebServiceActivity.kt
+++ app/src/main/java/fr/epsi/cafeteria/WebServiceActivity.kt
@@ -14,47 +14,5 @@ class WebServiceActivity : BaseActivity() {
         setContentView(R.layout.activity_web_service)
         setHeaderTitle("Rayons")
         showBack()
-
-        val products = arrayListOf<Product>()
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewProducts)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        val productAdapter = ProductAdapter(products)
-        recyclerView.adapter=productAdapter
-
-        val okHttpClient: OkHttpClient = OkHttpClient.Builder().build()
-        val requestUrl="http://djemam.com/epsi/categories.json"
-        val request = Request.Builder()
-            .url(requestUrl)
-            .get()
-            .cacheControl(CacheControl.FORCE_NETWORK)
-            .build()
-
-        okHttpClient.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                TODO("Not yet implemented")
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                val data = response.body?.string()
-
-                if(data!=null){
-                    val jsProducts = JSONObject(data)
-                    val jsArrayProducts= jsProducts.getJSONArray("items")
-                    for(i in 0 until jsArrayProducts.length()){
-                        val jsProduct = jsArrayProducts.getJSONObject(i)
-                        val product = Product(jsProduct.optString("title",""),
-                            jsProduct.optString("category_id",""),
-                            jsProduct.optString("products_url",""))
-                        products.add(product)
-                        Log.d("product",product.name)
-                    }
-                    Log.d("Product","${products.size}")
-                    runOnUiThread(Runnable {
-                        productAdapter.notifyDataSetChanged()
-                    })
-                }
-            }
-
-        })
     }
 }
