d3d88af4ecdb2eda66253481d77626e7962eefa4
aanan <anania.alexandre@gmail.com>
2021-12-30 21:46:25 +0100

    Add CategoriesActivities (formerly WebServiceActivity) by fetching datas from the giving url

---
+++ app/src/main/java/fr/epsi/cafeteria/CategoriesActivity.kt
@@ -0,0 +1,53 @@
+package fr.epsi.cafeteria
+
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.GsonBuilder
+import okhttp3.*
+import java.io.IOException
+
+class WebServiceActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_categories)
+        setHeaderTitle("Rayons")
+        showBack()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewCategories)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        fetchJson()
+    }
+
+    fun fetchJson() {
+        val okHttpClient: OkHttpClient = OkHttpClient.Builder().build()
+        val mRequestURL = "https://djemam.com/epsi/categories.json"
+        val request = Request.Builder()
+            .url(mRequestURL)
+            .get()
+            .cacheControl(CacheControl.FORCE_NETWORK)
+            .build()
+
+        okHttpClient.newCall(request).enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                val data = response.body?.string()
+                val gson = GsonBuilder().create()
+                val categoryList = gson.fromJson(data, CategoryList::class.java)
+                val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewCategories)
+                val categoryAdapter = CategoryAdapter(categoryList)
+                runOnUiThread {
+                    recyclerView.adapter=categoryAdapter
+                }
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                println("Failed to execute request")
+            }
+        })
+    }
+}
+
+class CategoryList (val items: List<Category>)
+
+class Category(val category_id: String, val title: String, val products_url: String)
