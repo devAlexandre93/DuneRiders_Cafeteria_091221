ff3bcdfb940a4fdc9dfa8dc2c92a4866bead0561
aanan <anania.alexandre@gmail.com>
2022-01-01 18:07:56 +0100

    End of the ProductDetailsActivity

--- app/src/main/java/fr/epsi/cafeteria/ProductDetailsActivity.kt
+++ app/src/main/java/fr/epsi/cafeteria/ProductDetailsActivity.kt
@@ -1,6 +1,9 @@
 package fr.epsi.cafeteria
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import com.squareup.picasso.Picasso
 
 class ProductDetailsActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -8,5 +11,14 @@ class ProductDetailsActivity : BaseActivity() {
         setContentView(R.layout.activity_product_details)
         intent.getStringExtra("title")?.let { setHeaderTitle(it) }
         showBack()
+
+        val imgView = findViewById<ImageView>(R.id.imgViewProductDetails)
+        val urlImage = intent.getStringExtra("productUrl")
+        Picasso.get().load(urlImage).into(imgView)
+
+        val txtView = findViewById<TextView>(R.id.txtViewProductDetails)
+        val description = intent.getStringExtra("description")
+        txtView.text = description
+
     }
 }
--- readme.md
+++ readme.md
@@ -4,3 +4,4 @@ Mini projet réalisé par une équipe de 3 personnes dans le cadre du Bachelor "
 
 - Lien vers email
 - Rectifier l'erreur "No adapter attached"
+- Image ne s'affiche pour le produit Steaks poulet Igloo (mettre une image par défaut lorque pas d'image dispo)
